56ab7163f1acc744fc0f72a21d922625b5d77091
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2016-12-13 01:32:05 +0900

    課題１

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,16 @@
  * Created by okuhamamasakiyo on 2016/12/13.
  */
 public class Main {
-    public static void main (String arg[]){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String arg[]) {
+        String str = "3、14";
+        try {
+            System.out.println(str.length());
+            Double.parseDouble(str);
+        } catch (java.lang.NullPointerException er) {
+            System.out.println("NullPointerExceptionが発生しました");
+            er.printStackTrace();
 
+        }
     }
 }
+
