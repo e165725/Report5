1e83c09ee35f1408f806c7e6affa277a19f62e28
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2016-12-13 01:22:18 +0900

    課題１

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by okuhamamasakiyo on 2016/12/13.
+ */
+public class Main {
+    public static void main (String arg[]){
+        String str = null;
+        System.out.println(str.length());
+
+    }
+}
